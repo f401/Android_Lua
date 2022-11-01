db9a138d409aa4b907fc6d0f5c6a7433873b23a7
nanLianQing <71924344+f401@users.noreply.github.com>
2022-11-01 17:24:35 +0800

    Delete app/src/main/obj directory

--- app/src/main/obj/local/arm64-v8a/objs/bridge/cstdouterr.o.d
+++
@@ -1,4 +0,0 @@
-src/main/obj/local/arm64-v8a/objs/bridge/cstdouterr.o: \
-  src/main/jni/bridge/cstdouterr.c src/main/jni/bridge/../common.h
-
-src/main/jni/bridge/../common.h:
--- app/src/main/obj/local/armeabi-v7a/objs/bridge/cstdouterr.o.d
+++
@@ -1,4 +0,0 @@
-src/main/obj/local/armeabi-v7a/objs/bridge/cstdouterr.o: \
-  src/main/jni/bridge/cstdouterr.c src/main/jni/bridge/../common.h
-
-src/main/jni/bridge/../common.h:
