07ef0586e9555ac76bc57586cda6d95a8b6816b6
f401 <nanLianQing@outlook.com>
2023-04-16 07:50:35 +0000

    fix

--- app/src/main/jni/lua/Android.mk
+++ app/src/main/jni/lua/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := lua
 LOCAL_CFLAGS += -std=c99 -O2 -Wall -Wextra -DLUA_USE_DLOPEN
-LOCAL_SRC_FILES := lapi.c ldo.c loadlib.c ltablib.c lauxlib.c ldump.c lobject.c ltm.c lbaselib.c lfunc.c lopcodes.c lua.c lbitlib.c lgc.c loslib.c luac.c lcode.c linit.c lparser.c lundump.c lcorolib.c liolib.c lstate.c lutf8lib.c lctype.c llex.c lstring.c lvm.c ldblib.c lmathlib.c lstrlib.c lzio.c ldebug.c lmem.c ltable.c
+LOCAL_SRC_FILES := lapi.c lauxlib.c lbaselib.c lcode.c lcorolib.c lctype.c ldblib.c ldebug.c ldo.c ldump.c lfunc.c lgc.c linit.c liolib.c llex.c lmathlib.c lmem.c loadlib.c lobject.c lopcodes.c loslib.c lparser.c lstate.c lstring.c lstrlib.c ltable.c ltablib.c ltm.c lua.c luac.c lundump.c lutf8lib.c lvm.c lzio.c
 LOCAL_LDLIBS += -ldl -L$(SYSROOT)/usr/lib -llog
 #LOCAL_ARM_MODE := arm
 include $(BUILD_SHARED_LIBRARY) 
