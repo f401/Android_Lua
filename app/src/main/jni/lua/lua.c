6e805340f9701ed03bed5da6a128d4745689232f
f401 <nanLianQing@outlook.com>
2023-04-16 07:57:08 +0000

    update

--- app/src/main/jni/lua/lua.c
+++ app/src/main/jni/lua/lua.c
@@ -647,7 +647,7 @@ static int pmain (lua_State *L) {
 }
 
 
-int main (int argc, char **argv) {
+int lua54_main (int argc, char **argv) {
   int status, result;
   lua_State *L = luaL_newstate();  /* create state */
   if (L == NULL) {
