521dd361d4675fb665fcd01986008cbfc4911bf5
f401 <nanLianQing@outlook.com>
2023-06-30 05:26:40 +0000

    update

---
+++ app/src/main/java/net/fred/lua/common/foreign/Pointer.java
@@ -0,0 +1,32 @@
+package net.fred.lua.common.foreign;
+
+public class Pointer {
+    private long addr;
+
+    public Pointer(long addr) {
+        this.addr = addr;
+    }
+
+    public long get() {
+        return addr;
+    }
+
+    public void set(long addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pointer)) return false;
+
+        Pointer pointer = (Pointer) o;
+
+        return addr == pointer.addr;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (addr ^ (addr >>> 32));
+    }
+}
