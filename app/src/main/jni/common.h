fba47edde07bdaa3de794c74b8ecf999d051d724
nanLianQing <71924344+f401@users.noreply.github.com>
2023-07-13 23:33:00 +0800

    Update common.h

--- app/src/main/jni/common.h
+++ app/src/main/jni/common.h
@@ -1,7 +1,7 @@
 #ifndef __COMMON_H_
 #define __COMMON_H_
 
-#define ptr_to_jlong(ptr) ((jlong)ptr)
-#define jlong_to_ptr(ptr, type) ((type*)ptr)
+#define ptr_to_jlong(ptr) ((jlong)(unsigned long long)(ptr))
+#define jlong_to_ptr(ptr, type) ((type*)(unsigned long long)(ptr))
 
 #endif
