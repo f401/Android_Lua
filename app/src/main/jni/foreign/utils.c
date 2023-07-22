428b23b62307a9c83a8ccf136174c217ae68d9c8
f401 <nanLianQing@outlook.com>
2023-07-22 14:40:03 +0000

    Update

--- app/src/main/jni/foreign/utils.c
+++ app/src/main/jni/foreign/utils.c
@@ -10,43 +10,48 @@ static jclass class_pointer;
 static jmethodID method_logger_info;
 static jmethodID method_logger_error;
 static jmethodID method_pointer_from;
-
 static jmethodID method_pointer_get;
 
-#define GET_CLASS(ENV, NAME, FULL_NAME) \
+#define FIND_CLASS(ENV, NAME, FULL_NAME) \
 if (class_##NAME == NULL || (*ENV)->IsSameObject(ENV, class_##NAME, NULL)) { \
-     jclass needleClass = (*ENV)->FindClass(ENV, FULL_NAME);                      \
-     if (needleClass == NULL) throwNativeException(ENV, "Failed to init class "#FULL_NAME); \
-     class_##NAME = (*ENV)->NewWeakGlobalRef(ENV, needleClass);                           \
+     jclass needleClass = (*ENV)->FindClass(ENV, FULL_NAME);                 \
+     if (needleClass == NULL) {                                              \
+        throwNativeException(ENV, "Failed to init class "#FULL_NAME);        \
+     } else {                                                                \
+        class_##NAME = (*ENV)->NewWeakGlobalRef(ENV, needleClass);           \
+     }                                                                       \
 }
 
-#define GET_STATIC_METHOD(ENV, CLASS, SIMPLE_NAME, NAME, DESC) \
+#define FIND_STATIC_METHOD(ENV, CLASS, SIMPLE_NAME, NAME, DESC) \
 if (method_##CLASS##_##SIMPLE_NAME == NULL || (*ENV)->IsSameObject(ENV, method_##CLASS##_##SIMPLE_NAME, NULL)) { \
     jmethodID __needleMethod = (*ENV)->GetStaticMethodID(ENV, class_##CLASS, NAME, DESC);                        \
-    if (__needleMethod == NULL) throwNativeException(ENV, "Failed to init class "#NAME);\
-    method_##CLASS##_##SIMPLE_NAME = (*ENV)->NewWeakGlobalRef(ENV, __needleMethod);                           \
+    if (__needleMethod == NULL) {                            \
+        throwNativeException(ENV, "Failed to init class "#NAME);\
+    } else {\
+        method_##CLASS##_##SIMPLE_NAME = (*ENV)->NewWeakGlobalRef(ENV, __needleMethod);                              \
+    }                                                 \
 }
 
 
-#define GET_CLASS_LOGGER(ENV, IF_FAILED_RETURN) \
-GET_CLASS(env, logger, "net/fred/lua/common/Logger"); \
+#define LOAD_CLASS_LOGGER(ENV, IF_FAILED_RETURN) \
+FIND_CLASS(env, logger, "net/fred/lua/common/Logger"); \
 IF_NULL_RETURN(class_logger, IF_FAILED_RETURN)
 
-#define GET_CLASS_POINTER(ENV, IF_FAILED_RETURN) \
-GET_CLASS(ENV, pointer, "net/fred/lua/foreign/Pointer"); \
+#define LOAD_CLASS_POINTER(ENV, IF_FAILED_RETURN) \
+FIND_CLASS(ENV, pointer, "net/fred/lua/foreign/Pointer"); \
 IF_NULL_RETURN(class_pointer, IF_FAILED_RETURN)
 
 void logger_info(JNIEnv *env, const char *msg) {
-    GET_CLASS_LOGGER(env,);
-    GET_STATIC_METHOD(env, logger, info, "i", "(Ljava/lang/String;)V");
+    LOAD_CLASS_LOGGER(env,);
+    FIND_STATIC_METHOD(env, logger, info, "i", "(Ljava/lang/String;)V");
     IF_NULL_RETURN(method_logger_info,);
     (*env)->CallStaticVoidMethod(env, class_logger, method_logger_info,
                                  (*env)->NewStringUTF(env, msg));
 }
 
 void logger_error(JNIEnv *env, const char *msg) {
-    GET_CLASS_LOGGER(env,);
-    GET_STATIC_METHOD(env, logger, error, "e", "(Ljava/lang/String;)V");
+    LOAD_CLASS_LOGGER(env,);
+    FIND_STATIC_METHOD(env, logger, error, "e", "(Ljava/lang/String;)V");
     IF_NULL_RETURN(method_logger_error,);
     (*env)->CallStaticVoidMethod(env, class_logger, method_logger_error,
                                  (*env)->NewStringUTF(env, msg));
@@ -58,15 +63,15 @@ void throwNativeException(JNIEnv *env, const char *msg) {
 }
 
 jobject pointer_create(JNIEnv *env, void *needle) {
-    GET_CLASS_POINTER(env, NULL);
-    GET_STATIC_METHOD(env, pointer, from, "from", "(J)Lnet/fred/lua/foreign/Pointer;");
+    LOAD_CLASS_POINTER(env, NULL);
+    FIND_STATIC_METHOD(env, pointer, from, "from", "(J)Lnet/fred/lua/foreign/Pointer;");
     IF_NULL_RETURN(method_pointer_from, NULL);
     return (*env)->CallStaticObjectMethod(env, class_pointer, method_pointer_from,
                                           ptr_to_jlong(needle));
 }
 
 void *pointer_get_from(JNIEnv *env, jobject obj) {
-    GET_CLASS_POINTER(env, NULL);
+    LOAD_CLASS_POINTER(env, NULL);
     if (method_pointer_get == NULL || (*env)->IsSameObject(env, method_pointer_get, NULL)) {
         jmethodID id = (*env)->GetMethodID(env, class_pointer, "get", "()J");
         if (id == NULL) {
