27613c6db585d5a7a2444ac135f43b3e4648ad05
f401 <nanLianQing@outlook.com>
2023-08-10 18:50:41 +0800

    Update

--- app/src/main/java/net/fred/lua/ui/view/FreeScrollView.java
+++ app/src/main/java/net/fred/lua/ui/view/FreeScrollView.java
@@ -3,23 +3,33 @@ package net.fred.lua.ui.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.widget.Scroller;
+import android.widget.OverScroller;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import net.fred.lua.common.Logger;
+import net.fred.lua.common.utils.MathUtils;
+
+// Based on https://github.com/TIIEHenry/CodeEditor/blob/master/CodeEditor/src/main/java/tiiehenry/code/view/TouchNavigationMethod.java
 public class FreeScrollView extends View {
+    public static final int DEFAULT_BOTTOM_GAP_SIZE = MathUtils.dp2px(400);
+
+    public static final float SCALE_MAX = 2.0f;
+    public static final float SCALE_MIN = 0.5f;
+
     private GestureDetector mGestureDetector;
     private ScaleGestureDetector mScaleGestureDetector;
-    private Scroller mScroller;
-    private Matrix mMatrix;
+    private OverScroller mScroller;
+    private float mScaleFactor;
+    private float mScaleFocusX, mScaleFocusY;
 
     public FreeScrollView(Context context) {
         super(context);
@@ -47,11 +57,33 @@ public class FreeScrollView extends View {
         mGestureDetector = new GestureDetector(context, touchNavigation);
         mScaleGestureDetector = new ScaleGestureDetector(context, touchNavigation);
 
-        mScroller = new Scroller(context);
-        mMatrix = new Matrix();
+        mScroller = new OverScroller(context);
 
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
+
+        mScaleFactor = 1.0f;
+        mScaleFocusX = mScaleFocusY = 0.0f;
+
+        setFocusable(true);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(useAllDimensions(widthMeasureSpec),
+                useAllDimensions(heightMeasureSpec));
+    }
+
+    private int useAllDimensions(int spec) {
+        int mode = MeasureSpec.getMode(spec);
+        int size = MeasureSpec.getSize(spec);
+
+        if (MeasureSpec.UNSPECIFIED == mode) {
+            size = Integer.MAX_VALUE;
+            Logger.w("MeasureSpec cannot be UNSPECIFIED. Setting dimensions to max.");
+        }
+
+        return size;
     }
 
     @Override
@@ -71,21 +103,51 @@ public class FreeScrollView extends View {
         return true;
     }
 
+    protected void onUp() {
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        canvas.scale(mScaleFactor, mScaleFactor, mScaleFocusX, mScaleFocusY);
+        super.onDraw(canvas);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, 200, mPaint);
+        canvas.restore();
+    }
+
+    // --------------------------------------------------------- Scroll And Scale ----------------------------------------//
+
     public boolean isScrolling() {
         return !mScroller.isFinished();
     }
 
     public void stopScroll() {
-        mScroller.forceFinished(true);
+        if (!mScroller.isFinished()) {
+            mScroller.abortAnimation();
+        }
     }
 
+
+    /**
+     * This includes rolling boundary check.
+     * When exceeding the boundary. Will use maximum(minimum) values.
+     */
     @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.concat(mMatrix);
-        super.onDraw(canvas);
-    }
+    public void scrollTo(int x, int y) {
+        final int maxX = Math.max(getScrollX(), getMaxScrollX());
+        if (x < 0) {
+            x = 0;
+        } else if (x > maxX) {
+            x = maxX;
+        }
 
-    protected void onUp() {
+        final int maxY = Math.max(getScrollY(), getMaxScrollY());
+        if (y < 0) {
+            y = 0;
+        } else if (y > maxY) {
+            y = maxY;
+        }
+        super.scrollTo(x, y);
     }
 
     protected int getMaxScrollX() {
@@ -96,11 +158,26 @@ public class FreeScrollView extends View {
         return Integer.MAX_VALUE;
     }
 
+    /**
+     * Converts a x-coordinate from screen coordinates to local coordinates,
+     * excluding padding
+     */
+    public int screenToViewX(int x) {
+        return x - getPaddingLeft() + getScrollX();
+    }
+
+    /**
+     * Converts a y-coordinate from screen coordinates to local coordinates,
+     * excluding padding
+     */
+    public int screenToViewY(int y) {
+        return y - getPaddingTop() + getScrollY();
+    }
+
     public class TouchNavigation extends GestureDetector.SimpleOnGestureListener implements ScaleGestureDetector.OnScaleGestureListener {
         @Override
         public boolean onScroll(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
-            mMatrix.postTranslate(-distanceX, -distanceY);
-            invalidate();
+            scrollBy((int) distanceX, (int) distanceY);
             return true;
         }
 
@@ -125,9 +202,16 @@ public class FreeScrollView extends View {
 
         @Override
         public boolean onScale(@NonNull ScaleGestureDetector detector) {
-            float scale = detector.getScaleFactor();
-            mMatrix.postScale(scale, scale, detector.getFocusX(), detector.getFocusY());
-            invalidate();
+            mScaleFactor *= detector.getScaleFactor();
+            if (mScaleFactor > SCALE_MAX) {
+                mScaleFactor = SCALE_MAX;
+            } else if (mScaleFactor < SCALE_MIN) {
+                mScaleFactor = SCALE_MIN;
+            }
+            mScaleFocusX = detector.getFocusX();
+            mScaleFocusY = detector.getFocusY();
+            Log.i("DEBUG", "Scale factor: " + mScaleFactor);
+            postInvalidate();
             return true;
         }
 
@@ -141,4 +225,5 @@ public class FreeScrollView extends View {
 
         }
     }
+
 }
